bbd33118ca77e8c91159095ef333176c9b5864a7
Alex Bujenita <dekc89@gmail.com>
2020-07-01 21:30:32 +0100

    appending and using files as iterators

---
+++ files/append.py
@@ -0,0 +1,10 @@
+g = open('wasteland', mode='at', encoding='utf-8')
+g.writelines(
+    [
+        'something\n',
+        'asdklasdas  sasdasdadn in\n',
+        'asdai iji jia\n'
+    ]
+)
+
+g.close()
