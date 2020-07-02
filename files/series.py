50492831196e9bb63478e62ff8eb5c9bbb7169a4
Alex Bujenita <dekc89@gmail.com>
2020-07-02 21:01:41 +0100

    file handling use with blocks

--- files/series.py
+++ files/series.py
@@ -6,15 +6,19 @@ import sys
 def read_series(filename):
     try:
         f = open(filename, mode='rt', encoding='utf-8')
-        return [ int(line.strip()) for line in f ]
+        return [int(line.strip()) for line in f]
         # series = []
         # for line in f:
         #     a = int(line.strip())
         #     series.append(a)
     finally:
         f.close()
-    return series
+
+
+def read_series_2(filename):  # using with block
+    with open(filename, mode='rt', encoding='utf-8') as f:
+        return [int(line.strip()) for line in f]
 
 
 if __name__ == "__main__":
-    read_series(filename=sys.argv[1])
+    read_series_2(filename=sys.argv[1])
